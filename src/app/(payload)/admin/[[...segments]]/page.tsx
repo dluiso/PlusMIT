0db16414f8c8b969edb85ca9163ae0d074b3e9c3
DLuiso <DLuiso@users.noreply.github.com>
2026-06-26 22:19:26 -0500

    Use generated Payload admin import map

--- src/app/(payload)/admin/[[...segments]]/page.tsx
+++ src/app/(payload)/admin/[[...segments]]/page.tsx
@@ -1,6 +1,6 @@
 import configPromise from '@payload-config'
 import { RootPage, generatePageMetadata } from '@payloadcms/next/views'
-import { importMap } from './importMap'
+import { importMap } from '../importMap.js'
 
 type Args = {
   params: Promise<{ segments: string[] }>
